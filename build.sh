e0747bf9c38b3830cee62dcec8a6881e46633c57
AnthonyPico <baqueanton18@gmail.com>
2024-05-12 11:58:04 -0500

    first commit

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 pip install -r requirements.txt
@@ -10,7 +8,6 @@ python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
-  python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
 fi
-
 
