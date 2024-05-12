750f374bad31fa78edd297d19e129a8a5ca2bceb
AnthonyPico <baqueanton18@gmail.com>
2024-05-12 02:13:16 -0500

    first commit

---
+++ build.sh
@@ -0,0 +1,13 @@
+set -o errexit
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py makemigration --no-input
+python manage.py mygrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
